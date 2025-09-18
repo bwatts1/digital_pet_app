d84072ef15c4a823ab5e27bc51cd8135d7fd8d21
bwatts1 <wattsbraydon2002@gmail.com>
2025-09-18 18:46:34 -0400

    updated the app basic design

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: DigitalPetApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class DigitalPetApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: _TabsNonScrollableDemo(),
-      ),
-    );
-  }
+  _DigitalPetAppState createState() => _DigitalPetAppState();
 }
 
-class _TabsNonScrollableDemo extends StatefulWidget {
-  @override
-  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
-}
+class _DigitalPetAppState extends State<DigitalPetApp> {
+  String petName = "Your Pet";
+  int happinessLevel = 50;
+  int hungerLevel = 50;
 
-class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo> with SingleTickerProviderStateMixin, RestorationMixin {
-  late TabController _tabController;
-  final RestorableInt tabIndex = RestorableInt(0);
-
-  @override
-  String get restorationId => 'tab_non_scrollable_demo';
-
-  @override
-  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(tabIndex, 'tab_index');
-    _tabController.index = tabIndex.value;
+  void _playWithPet() {
+    setState(() {
+      happinessLevel += 10;
+      _updateHunger();
+    });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      initialIndex: 0,
-      length: 4,
-      vsync: this,
-    );
-    _tabController.addListener(() {
-      setState(() {
-        tabIndex.value = _tabController.index;
-      });
+  void _feedPet() {
+    setState(() {
+      hungerLevel -= 10;
+      _updateHappiness();
+    });
+  }
+
+  void _updateHappiness() {
+    if (hungerLevel < 30) {
+      happinessLevel -= 20;
+    } else {
+      happinessLevel += 10;
+    }
+  }
+
+  void _updateHunger() {
+    setState(() {
+      hungerLevel += 5;
+      if (hungerLevel > 100) {
+        hungerLevel = 100;
+        happinessLevel -= 20;
+      }
     });
   }
 
   @override
-  void dispose() {
-    _tabController.dispose();
-    tabIndex.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final tabs = ['Tab1', 'Tab2', 'Tab3', 'Tab4'];
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Tabs Demo'),
-        backgroundColor: Colors.blue,
-        bottom: TabBar(
-          controller: _tabController,
-          isScrollable: false,
-          tabs: [
-            for (final tab in tabs) Tab(text: tab),
-          ],
-        ),
+        title: Text('Digital Pet'),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Image.asset(
-                'image/bear.webp',
-                width: 300,
-                height: 300,
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Image.asset(
-                'image/cat.jpg',
-                width: 300,
-                height: 300,
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Image.asset(
-                'image/lion.jpg',
-                width: 300,
-                height: 300,
-              ),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: Center(
-              child: Image.asset(
-                'image/zebra.jpg',
-                width: 300,
-                height: 300,
-              ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.blue,
-        child: Row(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-              },
+          children: <Widget>[
+            Text(
+              'Name: $petName',
+              style: TextStyle(fontSize: 20.0),
             ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-              },
+            SizedBox(height: 16.0),
+            Text(
+              'Happiness Level: $happinessLevel',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Hunger Level: $hungerLevel',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: _playWithPet,
+              child: Text('Play with Your Pet'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _feedPet,
+              child: Text('Feed Your Pet'),
             ),
           ],
         ),
